4a99cd550f7d5bd67491df664a53e145ca1e3e73
Priyanshu <priya14anshu@gmail.com>
2024-02-27 15:02:10 +0530

    .

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { FiGithub } from "react-icons/fi";
+import { FaGithub } from "react-icons/fa";
+
+import { FaExternalLinkAlt } from "react-icons/fa";
+
 import "./Projects.css";
 
 export const ProjectCard = (params) => {
@@ -9,41 +14,47 @@ export const ProjectCard = (params) => {
         backgroundColor: "#1C1B23",
       }}
     >
-      <img src={params.image} class="card-img-top" alt="..."/>
-      <div class="card-body" >
-        <h7 class="card-title" style={{ color: "white" }}>
-          {params.name}
-        </h7>
+      <img src={params.image} class="card-img-top" alt="..." />
+      <div class="card-body">
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div>
+            <h7 class="card-title" style={{ color: "white" }}>
+              {params.name}
+            </h7>
+          </div>
+
+          <div>
+            <a
+              href={params.github}
+              style={{ fontSize: "2.4vh", color: "white" }}
+            >
+              <FaGithub />
+            </a>
+            {params.demo !== "" && (
+              <a
+                href={params.demo}
+                style={{
+                  fontSize: "2.3vh",
+                  textDecoration: "none",
+                  marginLeft: "3vh",
+                  color: "red",
+                }}
+              >
+                <FaExternalLinkAlt />
+              </a>
+            )}
+          </div>
+        </div>
+
         <p class="card-text" style={{ color: "#8B8A90", fontSize: "14px" }}>
           {params.description}
         </p>
-        <div style={{ display: "flex", flexWrap: "wrap", gap: "1.501vh" }}>
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
           {params.tags.map((tag) => (
-            <p className="tags">{tag}</p>
+            <p className="tags" style={{ marginRight: "9px" }}>
+              {tag}
+            </p>
           ))}
-        </div>
-        <div>
-          <a
-            href={params.github}
-            class="btn btn-outline-success"
-            style={{ width: "11vh", fontSize: "2vh", padding: "0.5vh" }}
-          >
-            GitHub
-          </a>
-          {params.demo !== "" && (
-            <a
-              href={params.demo}
-              class="btn btn-outline-warning"
-              style={{
-                width: "11vh",
-                fontSize: "2vh",
-                padding: "0.5vh",
-                marginLeft: "1vh",
-              }}
-            >
-              Live
-            </a>
-          )}
         </div>
       </div>
     </div>
